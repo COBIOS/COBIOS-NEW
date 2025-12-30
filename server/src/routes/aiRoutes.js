8c6901e11a29134ef3df1725bd4ff3d3895f6479
yateen <coder.yateen@gmail.com>
2025-12-30 16:53:37 +0530

    cors all and comment rate limit

--- server/src/routes/aiRoutes.js
+++ server/src/routes/aiRoutes.js
@@ -4,14 +4,14 @@ const aiController = require("../controllers/aiController");
 const auth = require("../middleware/auth");
 const roleGuard = require("../middleware/roleGuard");
 const validate = require("../middleware/validate");
-const { aiLimiter } = require("../middleware/rateLimiter");
+// const { aiLimiter } = require("../middleware/rateLimiter");
 const {
   generateWorkoutPlanValidator,
   generateDietPlanValidator,
 } = require("../validators/planValidator");
 
 router.use(auth);
-router.use(aiLimiter);
+// router.use(aiLimiter);
 
 router.post(
   "/generate-workout-plan",
